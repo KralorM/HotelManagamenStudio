14844417bf85e59a84992e1ea27e33248e385e89
KralorM <116275562+KralorM@users.noreply.github.com>
2023-06-14 19:09:11 +0200

    Update MainWindow.xaml.cs
    Dodano komentarze XML i naprawiono błąd z aktualizacją wierszy tabeli gości

--- TablesWindows_andXamlConfigs/MainWindow.xaml.cs
+++ TablesWindows_andXamlConfigs/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +16,7 @@ using System.Data.Entity;
 
 namespace HotelManagamenStudio
 {
-     /// <summary>
+    /// <summary>
     /// W konstruktorze Mainwindow inicjalizowane są 
     /// obiekty CollectionViewSource i przypisywane są im źródła danych.
     /// </summary>
@@ -37,10 +37,17 @@ namespace HotelManagamenStudio
             var guest = from g in context.guests
                         select g;
 
-            
-            this.Resultguestgrid.ItemsSource = guest.ToList();
+
+            this.guestsDataGrid.ItemsSource = guest.ToList();
+
         }
 
+        /// <summary>
+        /// Metoda Window_Loaded jest wywoływana po załadowaniu okna i służy do ustawienia 
+        /// źródła danych dla niektórych kontrolkami w interfejsie użytkownika
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             // Load is an extension method on IQueryable,    
@@ -53,18 +60,24 @@ namespace HotelManagamenStudio
 
             // After the data is loaded, call the DbSet<T>.Local property    
             // to use the DbSet<T> as a binding source.   
+            
             guestViewSource.Source = context.guests.Local;
+            this.guestsDataGrid.ItemsSource = context.guests.Local;
+            // Load data by setting the CollectionViewSource.Source property:
+
         }
-        //private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
-        //{
-
-        //}
-
+        
+        /// <summary>
+        /// Metoda CommandBinding_Executed_1 obsługuje kliknięcie przycisku "Add" i dodaje nowego gościa do 
+        /// bazy danych na podstawie danych wprowadzonych przez użytkownika.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CommandBinding_Executed_1(object sender, ExecutedRoutedEventArgs e)
         {
             guests guests = new guests();
 
-            //guests.guest_id = GuestIDTextBox.Text.Trim();
+            guests.guest_id = Convert.ToInt32(GuestIDTextBox.Text.Trim());
             guests.first_name = FirstNameTextBox.Text.Trim();
             guests.last_name = LastNameTextBox.Text.Trim();
             guests.phone = phoneTextBox.Text.Trim();
@@ -80,17 +93,83 @@ namespace HotelManagamenStudio
             MessageBox.Show("Submitted succesfully!");
                 
         }
-
+        /// <summary>
+        /// Metoda CommandBinding_Executed_2 służy do aktualizowania danych gości, Na przykład
+        /// modyfikacji ich numeru telefonu adresu itd.Na początku deklarujemy obiekt kontekstu bazy danych hotel5
+        /// Następnie tworzony jest zapytanie do bazy danych, które pobiera gości na podstawie ich identyfikatora (guest_id). 
+        /// this.guestid.
+        /// Pobrany gość jest przypisywany do zmiennej guests przy użyciu metody FirstOrDefault(). 
+        ///Następnie sprawdzane jest, czy zmienna guests nie jest równa null, co oznacza, że gość został znaleziony w bazie danych.
+        ///Jeśli gość został znaleziony, to następuje aktualizacja jego danych na podstawie wartości wprowadzonych w polach tekstowych(FirstNameTextBox, LastNameTextBox, itd.).
+        ///Wywoływana jest metoda SaveChanges() na obiekcie hotel5 w celu zapisania zmian w bazie danych.
+        ///Na końcu wyświetlane jest okno dialogowe (MessageBox), które informuje użytkownika, że gość został zaktualizowany.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CommandBinding_Executed_2(object sender, ExecutedRoutedEventArgs e)
-        {
+        {   
+           hotel5Entities hotel5 = new hotel5Entities();
+         
+            var r = from g in hotel5.guests
+                    where g.guest_id == this.guestid
+                    select g;
 
+
+            guests guests = r.SingleOrDefault();
+
+            if (guests != null)
+            {
+
+
+                guests.first_name = this.FirstNameTextBox.Text;
+                guests.last_name = this.LastNameTextBox.Text;
+                guests.phone = this.phoneTextBox.Text;
+                guests.adress = this.addressTextBox.Text;
+                guests.nationality = this.NationalityTextBox.Text;
+                hotel5.SaveChanges();
+                MessageBox.Show("Selected guest have been updated!");
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a guest to update");
+            }
+
+          
         }
+
+        
 
         private void CommandBinding_Executed_3(object sender, ExecutedRoutedEventArgs e)
         {
-            
+            if (MessageBox.Show("Are you sure you want to delete this row?", "EF CRUD Operation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                using (hotel5Entities hotel = new hotel5Entities())
+                {
+                    var gur = guestViewSource.View.CurrentItem as guests;
+
+                    var gust = (from g in context.guests
+                                where g.guest_id == gur.guest_id
+                                select g).FirstOrDefault();
+
+                    if (gust != null)
+                    {
+                        context.guests.Remove(gust);
+                        context.SaveChanges();
+                        guestViewSource.View.Refresh();
+
+                    }
+
+                }
+            }
         }
 
+        /// <summary>
+        /// Metoda CommandBinding_Executed_4 obsługuje kliknięcie przycisku 
+        /// "Cancel" i czyści wprowadzone dane, przywracając widok tabeli gości.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CommandBinding_Executed_4(object sender, ExecutedRoutedEventArgs e)
         {
             GuestIDTextBox.Text = "";
@@ -103,6 +182,12 @@ namespace HotelManagamenStudio
             existingGuestGrid.Visibility = Visibility.Visible;
         }
 
+        /// <summary>
+        /// Metoda deletebttn_bookings_Click obsługuje kliknięcie
+        /// przycisku "Delete" i usuwa zaznaczonego gościa z bazy danych.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Delete_Button_Click(object sender, RoutedEventArgs e)
         {
             if (MessageBox.Show("Are you sure you want to delete this row?","EF CRUD Operation",MessageBoxButton.YesNo)==MessageBoxResult.Yes)
@@ -125,6 +210,55 @@ namespace HotelManagamenStudio
 
                 }
             }
+        }
+
+        private int guestid = 0;
+       
+
+        private void guestsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.guestsDataGrid.SelectedIndex >= 0)
+            {
+                if (this.guestsDataGrid.SelectedItems.Count >= 0)
+                {
+                    if (this.guestsDataGrid.SelectedItems[0].GetType() == typeof(guests))
+                    {   
+                       
+                        guests g = (guests)this.guestsDataGrid.SelectedItems[0];
+                        this.FirstNameTextBox.Text = g.first_name;
+                        this.LastNameTextBox.Text = g.last_name;
+                        this.phoneTextBox.Text = g.phone;
+                        this.addressTextBox.Text = g.adress;
+                        this.NationalityTextBox.Text = g.nationality;
+                        this.guestid = g.guest_id;
+                    }
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        /// Poniższe Metody odpowiadają za wyświetlanie nowych okien kolejno dla table rooms,Employee,bookings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_rooms(object sender, RoutedEventArgs e)
+        {
+            Rooms rooms = new Rooms();
+            rooms.Show();
+        }
+
+        private void Button_Click_emp(object sender, RoutedEventArgs e)
+        {
+            Employeewindow employeewindow = new Employeewindow();
+            employeewindow.Show();
+        }
+
+        private void Button_Click_bookings(object sender, RoutedEventArgs e)
+        {
+            Bookingswindow bookingswindow = new Bookingswindow();
+            bookingswindow.Show();
         }
     }
 }
